14deaab8839cdea2c442f67eca7c0dc00abf935e
Timur <86473514+mozex1@users.noreply.github.com>
2022-12-07 17:46:21 +0300

    logic highlight cells available for move

--- src/components/BoardComponent.tsx
+++ src/components/BoardComponent.tsx
@@ -1,21 +1,49 @@
 import React, { FC, useState, useEffect } from 'react';
 import { Board } from '../models/Board';
+import { Cell } from '../models/Cell';
 import CellComponent from './CellComponent';
 
 interface BoardProps {
     board: Board;
     setBoard: (board: Board) => void;
-}
+};
 
 const BoardComponent: FC<BoardProps> = ({ board, setBoard }) => {
+	const [ selectedCell, setSelectedCell ] = useState<Cell | null>(null);
+
+	function click(cell: Cell) {
+		if (selectedCell && selectedCell !== cell && selectedCell.figure?.canMove(cell)) {
+			selectedCell.moveFigure(cell);
+			setSelectedCell(null);
+		  } else {
+			setSelectedCell(cell);
+		  }
+	};
+
+	useEffect(() => {
+		highlightCells();
+	}, [selectedCell])
+
+	function highlightCells() {
+		board.highlightCells(selectedCell);
+		updateBoard();
+	};
+
+	function updateBoard(){
+		const newBoard = board.getCopyBoard();
+		setBoard(newBoard);
+	}
+
     return (
         <div className="board">
         {board.cells.map((row, index) =>
           <React.Fragment key={index}>
             {row.map(cell =>
               <CellComponent
+                click={click}
                 cell={cell}
                 key={cell.id}
+                selected={cell.x === selectedCell?.x && cell.y === selectedCell?.y}
               />
             )}
           </React.Fragment>
--- src/components/CellComponent.tsx
+++ src/components/CellComponent.tsx
@@ -2,15 +2,22 @@ import { FC } from 'react';
 import { Cell } from "../models/Cell";
 
 interface CellProps {
-    cell: Cell
-}
-
-const CellComponent: FC<CellProps> = ({cell}) => {
-    return (
-        <div className={['cell', cell.color].join(' ')}>
-            {cell.figure?.logo && <img src={cell.figure.logo} alt='figure' />}
-        </div>
-    )
+    cell: Cell;
+    selected: boolean;
+    click: (cell: Cell) => void;
 };
+
+const CellComponent: FC<CellProps> = ({cell, selected, click}) => {
+    return (
+      <div
+        className={['cell', cell.color, selected ? "selected" : ''].join(' ')}
+        onClick={() => click(cell)}
+        style={{background: cell.available && cell.figure ? 'green' : ''}}
+      >
+        {cell.available && !cell.figure && <div className={"available"}/>}
+        {cell.figure?.logo && <img src={cell.figure.logo} alt=""/>}
+      </div>
+    );
+  };
 
 export default CellComponent;
