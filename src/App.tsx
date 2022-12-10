8650e523a41e921bb9064ebf3068a8aeb8292a11
Timur <86473514+mozex1@users.noreply.github.com>
2022-12-10 19:54:05 +0300

    transfer of a move in the game

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,18 @@ import { useState, useEffect } from 'react';
 import './App.css';
 import BoardComponent from './components/BoardComponent';
 import { Board } from './models/Board';
+import { Colors } from './models/Colors';
+import { Player } from './models/Player';
 
 function App() {
 	const [ board, setBoard ] = useState(new Board());
+	const [ whitePlayer, setWhitePlayer] = useState(new Player(Colors.WHITE));
+	const [ blackPlayer, setBlackPlayer] = useState(new Player(Colors.BLACK));
+	const [ currentPlayer, setCurrentPlayer ] = useState<Player | null>(null);
 
 	useEffect(() => {
-		restart()
+		restart();
+		setCurrentPlayer(whitePlayer);
 	}, [])
 
 	function restart() {
@@ -15,13 +21,19 @@ function App() {
 		newBoard.initCells();
 		newBoard.addFigures();
 		setBoard(newBoard);
-	}
+	};
+
+	function swapPlayer() {
+		setCurrentPlayer(currentPlayer?.color === Colors.WHITE ? blackPlayer : whitePlayer);
+	};
 
 	return (
 	<div className="app">
 		<BoardComponent
 		board={board}
 		setBoard={setBoard}
+		currentPlayer={currentPlayer}
+		swapPlayer={swapPlayer}
 		/>
 	</div>
 	);
--- src/models/figures/Pawn.ts
+++ src/models/figures/Pawn.ts
@@ -11,8 +11,30 @@ export class Pawn extends Figure {
         this.name = FigureNames.PAWN;
     };
 
-    canMove(target: Cell) : boolean {
+    isFirstStep: boolean = true;
+
+    canMove(target: Cell): boolean {
         if(!super.canMove(target)) return false;
-        return true;
-    };
+        const direction = this.cell.figure?.color === Colors.BLACK ? 1 : -1
+        const firstStepDirection = this.cell.figure?.color === Colors.BLACK ? 2 : -2
+    
+        if ((target.y === this.cell.y + direction || this.isFirstStep
+            && (target.y === this.cell.y + firstStepDirection))
+          && target.x === this.cell.x
+          && this.cell.board.getCell(target.x, target.y).isEmpty()) {
+          return true;
+        }
+    
+        if(target.y === this.cell.y + direction
+        && (target.x === this.cell.x + 1 || target.x === this.cell.x - 1)
+        && this.cell.isEnemy(target)) {
+          return true;
+        }
+    
+        return false;
+      }
+    movieFigure(target: Cell) {
+        super.canMove(target);
+        this.isFirstStep = false;
+    }
 };
